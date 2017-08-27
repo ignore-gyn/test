b8c4e0530234d4e8e4a7b467f58aeb4a67fa3ac8
ignore-gyn <ignore.gyn@gmail.com>
2017-08-28 00:36:38 +0900

    Add files via upload

--- Test.java
+++ Test.java
@@ -10,31 +10,35 @@ public class Test {
 		decodeLong();
 	}
 
-	public static Long encode1() {
-		byte[] stream = {(byte) 0xAB, (byte) 0xCD, (byte) 0xEF, 0x12};
+	public static Long encodeLong1() {
+		byte[] stream = {(byte) 0xAB, (byte) 0xCD, (byte) 0xEF, 0x12}; // 2882400018
 
-		Long l = null;
-		String s = new String(stream);
-		l = new Long(Long.decode("0xABCDEF12"));
-		System.out.print(l);
-		return l;
-	}
 
-	public static Long encode2() {
-		byte[] stream = {(byte) 0xAB, (byte) 0xCD, (byte) 0xEF, 0x12};
+		ByteBuffer buf = ByteBuffer.allocate(4).put(stream);
+		// int & long
+		// (long)int & long
+		// (long)-23 = -23L
+		Long l = -4L;
+//		Long l = buf.getInt(0) & 0x7FFFFFFFFFFFFFFFL;
+//		long l = buf.getInt(0) | 0x0000000000000000L;
 
-		ByteBuffer buffer = ByteBuffer.allocate(4);
-		Integer i = buffer.put(stream).getInt(0);
-		Long l = (long) i;
-		l &= 0x00000000FFFFFFFFL;
-		System.out.print(l);
+		Long l1 = (long) ByteBuffer.allocate(4).put(stream).getInt(0);
+		Long l2 = ByteBuffer.allocate(8).putInt(0).put(stream).getLong(0);
+
+		System.out.println(Integer.toBinaryString(buf.getInt(0)));
+		System.out.println(Long.toBinaryString(l));
+		System.out.println(l);
+		System.out.println(l1);
+		System.out.println(l2);
 		return l;
 	}
 
 	public static Long encodeLong() {
 		byte[] stream = {(byte) 0xAB, (byte) 0xCD, (byte) 0xEF, 0x12}; // 2882400018
 
-		Long l = ByteBuffer.allocate(4).put(stream).getInt(0) & 0x00000000FFFFFFFFL;
+
+		Long l = ByteBuffer.allocate(8).putInt(0).put(stream).getLong(0);
+//		Long l = ByteBuffer.allocate(4).put(stream).getInt(0) & 0x00000000FFFFFFFFL;
 
 		System.out.println(l);
 		return l;
@@ -52,12 +56,10 @@ public class Test {
 	public static byte[] decodeLong() {
 		Long l = 2882400018L;
 
-		Integer i = l.intValue();
-
-		byte[] buf = ByteBuffer.allocate(4).putInt(i).array();
-		for (int j = 0; j < 4; j++) {
-			System.out.print(String.format("%02X", buf[j]) + ' ');
-		}
+		byte[] buf = ByteBuffer.allocate(4).putInt(l.intValue()).array();
+//		for (int j = 0; j < 4; j++) {
+//			System.out.print(String.format("%02X", buf[j]) + ' ');
+//		}
 		return buf;
 	}
 }
