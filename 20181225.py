56af9bdeae849cb344a96b0436f53ecc41f0f5dd
ignore-gyn <ignore.gyn@gmail.com>
2018-12-25 03:18:40 +0900

    Add files via upload

---
+++ 20181225.py
@@ -0,0 +1,16 @@
+param = {}
+
+disp_format = "%7.2f"
+
+
+
+param["max"] = (disp_format % field_max).strip()
+param["min"] = 
+
+param["disp_value"] = 
+value_list = param["disp_value"].split(",")
+
+map(lambda x: s.strip('" '), s.split(","))
+
+param["flg_sens"]
+param["flg_sels"]
